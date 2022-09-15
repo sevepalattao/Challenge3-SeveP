4f24b0dcea559364732d90a34d94e9b99246c3c2
BigSev15 <palat008@umn.edu>
2022-09-15 14:44:40 -0500

    wrote js to generate password

--- script.js
+++ script.js
@@ -5,9 +5,37 @@ var generateBtn = document.querySelector("#generate");
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
-
   passwordText.value = password;
+}
 
+function generatePassword() {
+    var passLength = window.prompt("How long would you like your password to be? (min.8 - max.128)");
+    var lowerChoice = window.confirm("Would you like it to include lower case characters?");
+    var upperChoice = window.confirm("Would you like it to include upper case characters?");
+    var numericChoice = window.confirm("Would you like it to include numeric characters?");
+    var specialChoice = window.confirm("Would you like it to include special characters?");
+    var lowerCase = 'abcdefghijklmnopqrstuvwxyz';
+    var upperCase = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    var numeric = '0123456789';
+    var special = '!@#$%^&*_-+=(),./:;<>?[]{}~';
+    var passphrase = '';
+    var realPassword = '';
+    if (lowerChoice) {
+        passphrase = passphrase + lowerCase;
+    } 
+    if (upperChoice) {
+        passphrase = passphrase + upperCase;
+    } 
+    if (numericChoice) {
+        passphrase = passphrase + numeric;
+    } 
+    if (specialChoice) {
+        passphrase = passphrase + special;
+    }
+    for (var i = 0; i < passLength; i++) {
+        realPassword += passphrase.charAt(Math.floor(Math.random() * passphrase.length));
+    }
+    return realPassword;
 }
 
 // Add event listener to generate button
